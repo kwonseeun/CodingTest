cfa0cd42bc60515170e8083076957fae6a82a917
kwonseeun <0115km@naver.com>
2022-04-01 23:24:23 +0900

    1. 문자찾기

--- CodingTest/src/algorythm01/example01.java
+++ CodingTest/src/algorythm01/example01.java
@@ -8,18 +8,26 @@ import java.util.Scanner;
 public class example01 {
 	public int solution (String str, char t) {
 		int answer = 0;
+		// 대문자 변환 
+		str = str.toUpperCase();
+		t = Character.toUpperCase(t);
 		
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i)==t) answer++; {
+				
+			}
+			
+		}
 		return answer;
+
 	}
 	
 	public static void main(String[] args) {
 		
 		example01 e = new example01();
-		Scanner sc = new Scanner(System.in);
-		
+		Scanner sc = new Scanner(System.in);		
 		String str = sc.next();
 		char c = sc.next().charAt(0);
-		
 		System.out.println(e.solution(str, c));
 	}
 		
