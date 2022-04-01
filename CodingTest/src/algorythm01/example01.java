abc6c8946d1067354b8e9ae58244680a9605ea00
kwonseeun <0115km@naver.com>
2022-04-01 23:17:54 +0900

    example01 문자 찾기

--- CodingTest/src/algorythm01/example01.java
+++ CodingTest/src/algorythm01/example01.java
@@ -1,11 +1,26 @@
 package algorythm01;
 
+import java.util.Scanner;
+
 /**1. 문자 찾기 
  * 한 개의 문자열을 입력받고, 특정 문자열을 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지알아내는 
  * 프로그램, 대소문자를 구분하지 않고 문자열의 길이는 100을 넘지 않는다. */
 public class example01 {
-	public static void main(String[] args) {
-		String str = "Computercooler";
+	public int solution (String str, char t) {
+		int answer = 0;
 		
+		return answer;
 	}
+	
+	public static void main(String[] args) {
+		
+		example01 e = new example01();
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		char c = sc.next().charAt(0);
+		
+		System.out.println(e.solution(str, c));
+	}
+		
 }
