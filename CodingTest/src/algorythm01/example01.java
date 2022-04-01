1e54076536d1a9d24f4c2eeb7aaf2bb2015230ff
kwonseeun <0115km@naver.com>
2022-04-01 23:11:04 +0900

    create

---
+++ CodingTest/src/algorythm01/example01.java
@@ -0,0 +1,11 @@
+package algorythm01;
+
+/**1. 문자 찾기 
+ * 한 개의 문자열을 입력받고, 특정 문자열을 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지알아내는 
+ * 프로그램, 대소문자를 구분하지 않고 문자열의 길이는 100을 넘지 않는다. */
+public class example01 {
+	public static void main(String[] args) {
+		String str = "Computercooler";
+		
+	}
+}
