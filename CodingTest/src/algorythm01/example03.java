a541a293dd19e3252796b4e36017634361dec488
kwonseeun <0115km@naver.com>
2022-04-10 22:53:10 +0900

    문장 속 단어 indexOf, subString 예제 추가

---
+++ CodingTest/src/algorythm01/example03.java
@@ -0,0 +1,24 @@
+package algorythm01;
+
+import java.util.Scanner;
+
+public class example03 {
+
+	public String solution(String str) {
+		String answer = "";
+		int m = Integer.MIN_VALUE; // 가장 작은 값으로 초기화 된다.
+		
+		return answer;
+	}
+	
+	
+	public static void main(String[] args) {
+
+		example03 e = new example03();
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		
+		
+	}
+
+}
