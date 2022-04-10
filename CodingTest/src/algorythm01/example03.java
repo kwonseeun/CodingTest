a29c8fd2c7a825b170cf2839172508cd4addb2d3
kwonseeun <0115km@naver.com>
2022-04-10 22:55:28 +0900

    foreach 추가

--- CodingTest/src/algorythm01/example03.java
+++ CodingTest/src/algorythm01/example03.java
@@ -7,7 +7,10 @@ public class example03 {
 	public String solution(String str) {
 		String answer = "";
 		int m = Integer.MIN_VALUE; // 가장 작은 값으로 초기화 된다.
-		
+		String[] s = str.split("");
+		for (String x : s) {
+			System.out.println(x);
+		}
 		return answer;
 	}
 	
