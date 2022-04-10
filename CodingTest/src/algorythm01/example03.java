505ff26e36aca1f3fea610e41343a65b47fd61b2
kwonseeun <0115km@naver.com>
2022-04-10 22:58:34 +0900

    주석 추가

--- CodingTest/src/algorythm01/example03.java
+++ CodingTest/src/algorythm01/example03.java
@@ -9,7 +9,11 @@ public class example03 {
 		int m = Integer.MIN_VALUE; // 가장 작은 값으로 초기화 된다.
 		String[] s = str.split("");
 		for (String x : s) {
-			System.out.println(x);
+			int len = x.length(); // 각각 길이의 단어가 len 에 들어가고 
+			if (len > m) { // len 이 m보다 크면 
+				m = len; // m 으로 갱신이 된다 (최대값 구하는 알고리즘)
+				answer = x;// 최대값이발견될 때 마다 answer 에 넣어준다 
+			}
 		}
 		return answer;
 	}
