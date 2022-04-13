bc9461a12844c2da988d5d5dbadada16f9af36f8
kwonseeun <0115km@naver.com>
2022-04-13 22:53:10 +0900

    add

--- CodingTest/src/algorythm01/example04.java
+++ CodingTest/src/algorythm01/example04.java
@@ -1,10 +1,28 @@
 package algorythm01;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class example04 {
 
+	public ArrayList<String> solution(int n, String[] str) {
+		ArrayList<String> answer = new ArrayList<>();
+		
+		return answer;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		example04 ex = new example04();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String[] str = new String[n];
+		for (int i = 0; i < str.length; i++) {
+			str[i] = sc.next();
+		}
+		for (String x : ex.solution(n, str)) {
+			System.out.println(x);
+		}
+		
 	}
 
 }
