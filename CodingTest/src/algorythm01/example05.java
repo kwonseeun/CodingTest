a098d12257082184a37d2b3b0dd9a3b89ac68927
kwonseeun <0115km@naver.com>
2022-04-15 23:37:38 +0900

    특정 문자열 뒤집기(toCharArray())

---
+++ CodingTest/src/algorythm01/example05.java
@@ -0,0 +1,20 @@
+package algorythm01;
+
+import java.util.Scanner;
+
+public class example05 {
+	public String solution(String str) {
+		String answer;
+		
+		return "";
+	}
+	
+	public static void main(String[] args) {
+		example05 ex = new example05();
+		Scanner sc = new Scanner(System.in);
+		
+		String str= sc.next();
+		System.out.println(ex.solution(str));
+	}
+
+}
