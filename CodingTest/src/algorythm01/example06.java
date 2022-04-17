477267a5ac03f0207601ff0fe6f1cb0f825d453c
kwonseeun <0115km@naver.com>
2022-04-17 22:54:07 +0900

    if add

--- CodingTest/src/algorythm01/example06.java
+++ CodingTest/src/algorythm01/example06.java
@@ -6,8 +6,8 @@ public class example06 {
 	public String solution(String str) {
 		String answer="";
 		for (int i = 0; i < str.length(); i++) {
-			System.out.println(str.charAt(i) + "" + i + "" +str.indexOf(str.charAt(i)));
-			
+			// System.out.println(str.charAt(i) + "" + i + "" +str.indexOf(str.charAt(i)));
+			if (str.indexOf(str.charAt(i)) == i) answer += str.charAt(i);
 		}
 		return answer;
 	}
