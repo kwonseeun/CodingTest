9d6a6bef90eed85835f65a8b5cf002689fceb056
kwonseeun <0115km@naver.com>
2022-04-19 22:11:55 +0900

    회문 문자열

--- CodingTest/src/algorythm01/example07.java
+++ CodingTest/src/algorythm01/example07.java
@@ -1,10 +1,17 @@
 package algorythm01;
 
 public class example07 {
-
+	public String solution(String str) {
+		String answer = "YES";
+		int len=str.length();
+		for (int i = 0; i < len/2; i++) 
+			if (str.charAt(i) != str.charAt(len-i-1)) return "NO";
+			
+		}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		example07 ex = new example07();
+		
 	}
 
 }
