753a453b6e4962b60959f33d60eb5cd19fab2e31
kwonseeun <0115km@naver.com>
2022-05-07 00:15:46 +0900

    주석 추가

--- CodingTest/src/algorythm01/example09.java
+++ CodingTest/src/algorythm01/example09.java
@@ -2,6 +2,9 @@ package algorythm01;
 
 import java.util.Scanner;
 
+/**문자와 숫자가 섞여있는 문자열이 주어지면 그 중 숫자만 추출하여 그 순서대로 자연수를 만든다.
+ * 만약 "tge0a1h205er" 에서 숫자만 추출하면 0,1,2,0,5이고 이것을 자연수로 만들면 1205 이다.
+ * 추출하여 만들어지는 자연수는 100,000,000을 넘지 않는다. */
 public class example09 {
 	public int solution(String s) {
 		int answer = 0;
