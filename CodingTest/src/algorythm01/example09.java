9596b3e254d58fceb03311d64bfe57415d788033
kwonseeun <0115km@naver.com>
2022-05-07 00:13:32 +0900

    solution 메소드 추가

--- CodingTest/src/algorythm01/example09.java
+++ CodingTest/src/algorythm01/example09.java
@@ -1,7 +1,17 @@
 package algorythm01;
 
 public class example09 {
-
+	public int solution(String s) {
+		int answer = 0;
+		for (char x: s.toCharArray()) {
+			if (x >= 48 && x<=57) 
+				answer=answer*10+(x-48); 
+		}
+		
+		return answer;
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
