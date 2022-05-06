8cc04d16474b6df9be7be7c93de667a4e68533b8
kwonseeun <0115km@naver.com>
2022-05-06 16:09:24 +0900

    main 함수 추가

--- CodingTest/src/algorythm01/example08.java
+++ CodingTest/src/algorythm01/example08.java
@@ -1,5 +1,7 @@
 package algorythm01;
 
+import java.util.Scanner;
+
 /**앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 팰린드롬이라고 한다.
  * 문자열이 입력되면 해당 문자열이 팰린드롬이면 "YES" 아니면 "NO"를 출력하는 프로그램을 작성하시오 
  * 단, 회문을 검사할 떄 알파벳만 가지고 회문을 검사하며, 대소문자를 구분하지 않는다.
@@ -15,5 +17,12 @@ public class example08 {
 		if (s.equals(tmp)) answer = "YES";
 		
 		return answer;
+	}
+	
+	public static void main(String[] args) {
+		example08 e = new example08();
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println(e.solution(str));
 	}
 }
