121b1a199d30a2b46aca128d3f2d964333c0682e
kwonseeun <0115km@naver.com>
2022-05-06 16:40:04 +0900

    1.문자 찾기 주석추가

--- CodingTest/src/algorythm01/example01.java
+++ CodingTest/src/algorythm01/example01.java
@@ -8,26 +8,22 @@ import java.util.Scanner;
 public class example01 {
 	public int solution (String str, char t) {
 		int answer = 0;
+		str = str.toUpperCase(); // string이 대문자화 된다.
+		t = Character.toUpperCase(t); 
 		
-		str = str.toUpperCase(); //str 이 대문자로 변환 
-		t = Character.toUpperCase(t); // 문자니까 character 클래스 메소드를 사용한다.
-		System.out.println(str + "" + t );
-		
-		for (int i = 0; i < str.length(); i++) { // str의 길이만큼 반복문 실행 , 하지만 0번째부터 시작이니까 안된 
-			if (str.charAt(i) == t)  answer ++; // 문자를 한개 한개 index 탐색 접근한다  
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == t) answer++; 
 		}
-		return answer; // answer리턴 하면서 갯수출력 
-
-	}
-	
-	public static void main(String[] args) {	
-		example01 e = new example01();
-		Scanner sc = new Scanner(System.in);	
 		
-		String str =sc.next(); // 문자열 하나를 읽어들인다.
-		char c = sc.next().charAt(0); // character 변수 문자 하나를 리턴한다 
+			return answer;
+	}	
+	
+	public static void main(String[] args) {
+		example01 e = new example01();
+		Scanner sc = new Scanner(System.in);
+		String str =sc.next(); // 문자열을 하나 읽어들인다. 
+		char c = sc.next().charAt(0); // 문자 '하나'를 가져와야 한다 
 		
 		System.out.println(e.solution(str, c));
 	}
-		
 }
