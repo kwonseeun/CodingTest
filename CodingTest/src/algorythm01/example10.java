9a772effc8f6f28d9e3a544cf1a556da181a184e
kwonseeun <0115km@naver.com>
2022-05-08 00:29:17 +0900

    10.문자거리

---
+++ CodingTest/src/algorythm01/example10.java
@@ -0,0 +1,26 @@
+package algorythm01;
+
+import java.util.Scanner;
+
+/**한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 t와 떨어진 최소거리를 
+ * 출력하는 프로그램을 작성하세요. 
+ * 
+ * 첫 번째 줄에 문자열 s와 문자t가 주어진다. 문자열과 문자는 소문자로만 주어진다.
+ * 문자열의 길이는 100을 넘지 않는다. */
+public class example10 {
+	public int solution() {
+		int answer = 0;
+		
+		
+		
+		return answer;
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		example10 e = new example10();
+		Scanner sc = new Scanner(System.in);
+	}
+
+}
