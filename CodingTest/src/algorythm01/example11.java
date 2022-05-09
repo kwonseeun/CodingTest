06cc5911bb229e142233f414d66c7db5276f02e5
kwonseeun <0115km@naver.com>
2022-05-09 22:00:58 +0900

    11.문자열 압축

---
+++ CodingTest/src/algorythm01/example11.java
@@ -0,0 +1,15 @@
+package algorythm01;
+
+/** 알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는 문자바로 오른쪽에
+ * 반복 횟수를 표기하는 방법으로 문자열을 압축하는 프로그램을 작성하시오. 
+ * 단 반복횟수가 1인경우 생략한다. */
+
+public class example11 {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
