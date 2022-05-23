f1bc539492524b38f9f5a123594490ac577993f7
kwonseeun <0115km@naver.com>
2022-05-23 09:41:41 +0900

    사이냅소프트 면접문제

--- CodingTest/src/example04.java
+++ CodingTest/src/example04.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class example04 {
 
 	public static void main(String[] args) {
@@ -17,7 +19,7 @@ public class example04 {
 		}
 		System.out.println("김씨와 이씨는 각각 몇명일까요? " + kimCnt+ "," +leeCnt);
 		
-		// 2번째 이재영이라는 이름은 몇번 반복되나 
+		// 2번 이재영이라는 이름은 몇번 반복되나 
 		int leejaeyoung = 0;
 		
 		for (int i = 0; i < nameList.length; i++) {
@@ -25,7 +27,23 @@ public class example04 {
 				leejaeyoung++;
 		}
 		System.out.println("이재영이라는 이름은 몇버 반복되나요 ? " + leejaeyoung);
-	}
-	
+		
+		
+		// 3번 중복을 제거한 이름을 출력하시오.
+		ArrayList<String> arrayList = new ArrayList<String>();
+		
+		for (int i = 0; i < nameList.length; i++) {
+			// 새로운 배열 리스트에 넣을 이름이 있는지 검사 후 없으면 더해준다
+			if (!arrayList.contains(nameList[i])) 
+				arrayList.add(nameList[i]);;
+			}
+			//배열 리스트에 정리 된 것을 다시 String 배열에 넣어두고 String 클래스 함수 사용
+			String newArray[] = arrayList.toArray(new String[arrayList.size()]);
+			
+			System.out.println("중복을 제거한 이름");
+			for (int i = 0; i < newArray.length; i++) {
+				System.out.println(newArray[i] + ((newArray.length == i +1)?"\n":", "));
+		}
+	}	
 }
  
