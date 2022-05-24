0efc62ad04ccc8ee384c861229284193c4e8be24
kwonseeun <0115km@naver.com>
2022-05-24 14:20:43 +0900

    싱글톤 패턴

---
+++ CodingTest/src/singletonPattern/App.java
@@ -0,0 +1,10 @@
+package singletonPattern;
+
+public class App {
+
+	public static void main(String[] args) {
+		Settings settings = Settings.getInstance();
+		Settings settings1 = Settings.getInstance();
+		System.out.println(settings == Settings.getInstance());
+	}
+}
---
+++ CodingTest/src/singletonPattern/Settings.java
@@ -0,0 +1,19 @@
+package singletonPattern;
+
+public class Settings {
+
+
+	private static Settings instance;
+	
+	private Settings() {
+		
+	}
+	
+	public static Settings getInstance() {
+		if(instance == null) {
+			instance = new Settings();
+			
+		}
+		return instance;
+	}
+}
