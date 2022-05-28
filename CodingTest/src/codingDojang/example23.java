a8ea729d69d1db5f5836da4ae9277729e7ced79c
kwonseeun <0115km@naver.com>
2022-05-29 00:01:12 +0900

    add

--- CodingTest/src/codingDojang/example23.java
+++ CodingTest/src/codingDojang/example23.java
@@ -9,7 +9,7 @@ public class example23 {
         int loop = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 1; j < arr.length - i; j++) {
-
+ 
                 if (arr[j - 1] > arr[j]) {
                     int temp = arr[j - 1];
                     arr[j - 1] = arr[j];
