78eb9180cd6cd6cda6231910910492b5e3c64882
kwonseeun <0115km@naver.com>
2022-10-19 21:43:50 +0900

    update

---
+++ CodingTest/src/Array_배열/example03.java
@@ -0,0 +1,19 @@
+package Array_배열;
+
+import java.util.Iterator;
+
+public class example03 {
+	public static void main(String[] args) {
+		
+		int arr[] = new int[10];
+		
+		int sum = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if ( i%2 == 0) {
+				
+			}
+		}
+		
+	}
+}
