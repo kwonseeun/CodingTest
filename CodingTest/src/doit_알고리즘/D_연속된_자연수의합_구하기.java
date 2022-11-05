72b3946d28440906be5db75c2d0d2dc4f5c9080f
kwonseeun <0115km@naver.com>
2022-11-05 23:53:41 +0900

    update

---
+++ CodingTest/src/doit_알고리즘/D_연속된_자연수의합_구하기.java
@@ -0,0 +1,14 @@
+package doit_알고리즘;
+
+import java.util.Scanner;
+
+public class D_연속된_자연수의합_구하기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int count = 1;
+		int start_index = 1;
+		int end_index = 1;
+		int sum = 1;
+	}
+}
