7c4d5a806585d700561b7992e4277e0bd7e67e3a
kwonseeun <0115km@naver.com>
2022-11-06 23:38:55 +0900

    update

--- CodingTest/src/String_문자열/B_단어의갯수.java
+++ CodingTest/src/String_문자열/B_단어의갯수.java
@@ -1,7 +1,20 @@
 package String_문자열;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class B_단어의갯수 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		  // 20210628 - 백준 1152번
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        //String str = st.nextToken();
+        //System.out.println(st.countTokens() + 1);
+        //이렇게 하면 런타임에러 발생.
+        
+        System.out.println(st.countTokens());
 	}
 }
