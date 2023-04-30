98fca1fbdcd34425d23d1924c6c55413b35da034
kwonseeun <0115km@naver.com>
2023-04-30 23:06:13 +0900

    프로그래머스_이상한 문자 만들기

---
+++ CodingTest/src/baekjoon/P_이상한_문자_만들기.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+public class P_이상한_문자_만들기 {
+	 public String solution(String s) {
+	        int index = 0;
+	        StringBuilder sb = new StringBuilder();
+
+	        for (int i = 0; i < s.length(); i++) {
+	            if(s.charAt(i) == ' ') {
+	                sb.append(' ');
+	                index = 0;
+	            } else {
+	                if(index % 2 != 0) {
+	                    sb.append(Character.toLowerCase(s.charAt(i)));
+	                    index ++;
+	                } else {
+	                    sb.append(Character.toUpperCase(s.charAt(i)));
+	                    index ++;
+	                }
+	            }
+	        }
+
+	        return sb.toString();
+	    }
+}
