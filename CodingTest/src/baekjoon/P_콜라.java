6954f279d3769eab71258f1935e443b3b2e29c91
kwonseeun <0115km@naver.com>
2023-05-05 23:45:58 +0900

    update

--- CodingTest/src/baekjoon/P_콜라.java
+++ CodingTest/src/baekjoon/P_콜라.java
@@ -6,7 +6,16 @@ public class P_콜라 {
 		int answer = 0;
 		
 		while (true) {
+			// 가지고 있는 빈 병 수가 a개보다 미만이면 return
+			if(n > a) {
+				break;
+			}
+			// 가지고 있는 빈 병 수가 a개보다 크면 시작
+			answer =+ ( n / a) * b;
+			int remain = n % a;
 			
+			n = (n / a) * b + remain;
 		}
+		return answer;
 	}
 }
