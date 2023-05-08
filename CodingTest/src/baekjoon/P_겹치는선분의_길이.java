c6e7f3f583bb35b2f7d71922b0e55f87c2c2431b
kwonseeun <0115km@naver.com>
2023-05-08 23:37:01 +0900

    프로그래머스_겹치는 선분의 길이

---
+++ CodingTest/src/baekjoon/P_겹치는선분의_길이.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+public class P_겹치는선분의_길이 {
+	 public int solution(int[][] lines) {
+	        int[] arr = new int[201];
+	        int answer = 0;
+	        for (int[] line : lines) {
+	            int a = line[0] + 100;
+	            int b = line[1] + 100;
+	            while (a<=b) {
+	                if (++arr[a++] == 2) answer++;
+	            }
+	        }
+	        return answer;
+	    }
+}
