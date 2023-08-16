c51a25cccb1d5d635848a6eb52af59bd015869e9
kwonseeun <0115km@naver.com>
2023-08-16 23:29:22 +0900

    프로그래머스_과일장수

---
+++ CodingTest/src/programmers/P_과일장수.java
@@ -0,0 +1,13 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class P_과일장수 {
+	 public int solution(int k, int m, int[] score) {
+	        int answer = 0;
+	        Arrays.sort(score);
+	        for(int i = score.length - m; i >= 0; i -= m)
+	            answer += score[i] * m;
+	        return answer;
+	    }
+}
