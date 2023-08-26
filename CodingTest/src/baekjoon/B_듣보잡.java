979c09d941fce3f63fe847f0565c9e42eb8cd9db
kwonseeun <0115km@naver.com>
2023-08-26 23:23:21 +0900

    백준_듣보잡

---
+++ CodingTest/src/baekjoon/B_듣보잡.java
@@ -0,0 +1,47 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class B_듣보잡 {
+	public static void main(String[] args) throws IOException {
+        // 0. 입력 및 초기화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // 1. hash에 듣도 못한 사람 반영
+        HashMap<String, Integer> hash = new HashMap<>();
+        for (int i = 0; i < N; i++)
+            hash.put(br.readLine(), 1);
+
+        // 2. hash에 보도 못한 사람이 존재하는지 확인
+        ArrayList<String> answer = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            String name = br.readLine();
+            if (hash.containsKey(name)) answer.add(name);
+        }
+
+        // 3. 출력
+        Collections.sort(answer);
+        bw.write(answer.size() + "\n");
+        for(int i = 0; i < answer.size(); i++){
+            bw.write(answer.get(i));
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
