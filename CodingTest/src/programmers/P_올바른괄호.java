63e058b9bc1ef068b92f652fe3eaa6080fe2ace8
kwonseeun <0115km@naver.com>
2023-09-06 23:29:34 +0900

    프로그래머스_올바른괄호

---
+++ CodingTest/src/programmers/P_올바른괄호.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class P_올바른괄호 {
+	  boolean solution(String s) {
+	        boolean answer = true;
+	        int a = 0;
+	        if(s.charAt(0) == ')') return false;
+	        else if(s.charAt(s.length()-1) =='(') return false;
+	        
+	        for(int i=0; i < s.length(); i++){
+	            if(s.charAt(i) == '(') a++;
+	            else if(s.charAt(i) == ')') a--;
+	            
+	            if(a < 0) return false;
+	        }
+	        //test
+	        return a == 0 ? true : false;
+	    }
+}
