819d77bc7b4e05691bcc4e0b89b5eee6e823bd14
kwonseeun <0115km@naver.com>
2023-09-25 21:41:38 +0900

    프로그래머스_모음사전

---
+++ CodingTest/src/programmers/P_모음사전.java
@@ -0,0 +1,26 @@
+package programmers;
+
+public class P_모음사전 {
+	  public String[] dict;
+	    public int cnt = 0, answer = 0;
+	    public int solution(String word) {
+	        dict = "AEIOU".split("");
+	        dfs("", word);
+	        
+	        return answer;
+	    }
+	    
+	    public void dfs(String cur, String word){
+	        if(cur.length() == 5 || cur.equals(word)){
+	            if(cur.equals(word)){
+	                answer = cnt;
+	            }
+	            return;
+	        }
+	        
+	        for(int i = 0; i < 5; i++){
+	            cnt++;
+	            dfs(cur+dict[i],word);
+	        }
+	    }
+}
