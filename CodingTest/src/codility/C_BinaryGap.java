c49fdb22eb3049820e7177457742dd10c6ef2229
kwonseeun <0115km@naver.com>
2023-10-01 22:27:44 +0900

    코딜리티_binaryGap

---
+++ CodingTest/src/codility/C_BinaryGap.java
@@ -0,0 +1,18 @@
+package codility;
+
+public class C_BinaryGap {
+	public int solution(int N) {
+        int result = 0;
+        int count = 0;
+        String str = Integer.toString(N , 2);
+        for(int i = 1; i < str.length(); i++) {
+            if(str.charAt(i) == '0') {
+                count++;
+            }else {
+                result = Math.max(result, count);
+                count = 0;
+            }
+        }
+        return result;
+    }
+}
