d70166a3b7e616ca83898f6fb89cb23f9f6ea31f
kwonseeun <0115km@naver.com>
2023-10-07 00:18:48 +0900

    코딜리티_MissingInteger

---
+++ CodingTest/src/codility/C_MissingInteger.java
@@ -0,0 +1,22 @@
+package codility;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class C_MissingInteger {
+	public int solution(int[] A) {
+		int result = 1;
+		
+		Set<Integer> set = new HashSet<>();
+		for (int i : A) if (i>0) set.add(i); {
+			
+			for (int i = 0; i < set.size()+1; i++) {
+				if(!set.contains(i)) {
+					result = i;
+					break;
+				}
+			}
+			return result;
+		}
+	}
+}
