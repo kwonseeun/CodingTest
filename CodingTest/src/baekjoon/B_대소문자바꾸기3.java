93d4c035ebf07b8ab49a8b36312d710a9e9de796
kwonseeun <0115km@naver.com>
2023-11-03 23:34:54 +0900

    백준_대소문자 바꾸기3

---
+++ CodingTest/src/baekjoon/B_대소문자바꾸기3.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_대소문자바꾸기3 {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        
+        char[] charArray = input.toCharArray();
+        for(int i=0; i<input.length(); i++){
+            char ch = input.charAt(i);
+            if('a' <= ch && ch <= 'z') charArray[i] = (char)(ch +'A'-'a');
+            else charArray[i] = (char)(ch +'a'-'A');            
+        }
+        
+        System.out.println(charArray);
+    }
+}
