ec682eaeb455177b9146aa1388dcd0d3c86127a0
kwonseeun <0115km@naver.com>
2023-11-07 23:19:38 +0900

    프로그래머스_더맵게

---
+++ CodingTest/src/programmers/P_프로그래머스.java
@@ -0,0 +1,34 @@
+package programmers;
+
+import java.util.PriorityQueue;
+
+public class P_프로그래머스 {
+	 int count;
+	    public int solution(int[] scoville, int k) {
+	        
+	        PriorityQueue<Integer> pq = new PriorityQueue();
+	        
+	        for(int i=0; i<scoville.length; i++){
+	            pq.offer(scoville[i]);
+	        }
+	        
+	        
+	        while(!pq.isEmpty()){
+	            if(pq.peek() >= k) return count;
+	            
+	            
+	            if(pq.size() <= 1) return -1;
+	            pq.offer(mix(pq.poll(), pq.poll()));
+	            count++;
+	           
+	        }
+	        
+	        return -1;    
+	        
+	    }
+	    
+	    
+	    public int mix(int first, int second){
+	        return (int)(first+(second*2));
+	    }
+}
