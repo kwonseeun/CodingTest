5ccb7d63bfb6932ea279acbd31871248dd79b13c
kwonseeun <0115km@naver.com>
2023-12-09 23:46:06 +0900

    백준_사분면 고르기

---
+++ CodingTest/src/baekjoon/B_사분면고르기.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B_사분면고르기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+
+		if(num1>0 && num2>0) {
+			System.out.println("1");
+		} else if(num1<0 && num2>0) {
+			System.out.println("2");
+		} else if(num1<0 && num2<0) {
+			System.out.println("3");
+		} else if(num1>0 && num2<0) {
+			System.out.println("4");
+		}
+	}
+}
