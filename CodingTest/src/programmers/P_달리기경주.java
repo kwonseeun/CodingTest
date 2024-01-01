8437520a431551436c60f24f1f159acc34dece5e
kwonseeun <0115km@naver.com>
2024-01-01 23:43:17 +0900

    P_달리기경주

---
+++ CodingTest/src/programmers/P_달리기경주.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.HashMap;
+
+public class P_달리기경주 {
+	 public String[] solution(String[] players, String[] callings) {
+	        HashMap<String, Integer> map = new HashMap<>();
+	        for(int i=0; i<players.length; i++){
+	            map.put(players[i], i);
+	        }
+	        
+	        for(int i=0; i<callings.length; i++){
+	            int rank = map.get(callings[i]);
+	            if(rank == 0){
+	                continue;
+	            }else{
+	                String temp = players[rank];
+	                map.put(temp, rank-1);
+	                players[rank] = players[rank-1];
+	                map.put(players[rank], rank);
+	                players[rank-1] = temp;
+	                
+	            }
+	        }
+	        return players;
+	    }
+}
