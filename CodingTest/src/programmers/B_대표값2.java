831330648eb93578a6761302c6aac5dcd7562bed
kwonseeun <0115km@naver.com>
2024-02-13 23:36:51 +0900

    B_대표값2

---
+++ CodingTest/src/programmers/B_대표값2.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B_대표값2 {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+	        int c = sc.nextInt();
+	        int d = sc.nextInt();
+	        int e = sc.nextInt();
+
+	        int avg = (a + b + c + d + e) / 5; // 평균
+
+	        int[] nums = {a, b, c, d, e};
+	        Arrays.sort(nums);
+	        int answer = nums[2]; // 중앙값
+
+
+	        System.out.print(avg + "\n" + answer);
+	        sc.close();
+	    }
+}
