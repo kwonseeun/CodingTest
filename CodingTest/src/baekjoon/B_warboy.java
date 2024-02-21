2764d432a597f563ca2a70ede9791155a4c4b8f4
kwonseeun <0115km@naver.com>
2024-02-21 23:59:21 +0900

    B_warboy

---
+++ CodingTest/src/baekjoon/B_warboy.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B_warboy {
+	 public static void main(String[] args){
+	        Scanner sc = new Scanner(System.in);
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+	        int c = sc.nextInt();
+
+	        int answer = (b / a) * 3 * c;
+	        System.out.println(answer);
+	        sc.close();
+	    }
+}
