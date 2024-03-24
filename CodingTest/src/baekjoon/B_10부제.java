322fc5e5d7ee04397ccc3b2d5eaa58f171666a3b
kwonseeun <0115km@naver.com>
2024-03-24 23:28:48 +0900

    B_10부제

---
+++ CodingTest/src/baekjoon/B_10부제.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B_10부제 {
+	public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(); // 주어진 날짜
+
+        int cnt = 0; // 위반하는 차량의 수
+
+        for (int i = 0; i < 5; i++) {
+            int car = sc.nextInt(); // 자동차의 일의자리 숫자
+
+            if (a == car){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
