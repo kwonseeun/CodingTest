0800dd47ada0db5b0fc8a75b2d05e0f0c70b043c
kwonseeun <0115km@naver.com>
2024-04-06 21:33:20 +0900

    P_x사이의 개수

---
+++ CodingTest/src/programmers/P_x사이의개수.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class P_x사이의개수 {
+	public int[] solution(String myString) {
+	    
+        String[] result = myString.split("x");
+        int N = myString.charAt(myString.length() - 1) == 'x' ? result.length + 1 : result.length;
+        int[] answer = new int[N];
+        
+        for (int i = 0;i < result.length;i++)
+            answer[i] = result[i].length();
+            
+        return answer;
+    }
+}
