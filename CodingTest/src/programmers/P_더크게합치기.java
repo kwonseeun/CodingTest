98ff1cf7026943f9f39504451bd8ea1c5724ce3c
kwonseeun <0115km@naver.com>
2024-04-28 23:24:55 +0900

    P_더 크게 합치기

---
+++ CodingTest/src/programmers/P_더크게합치기.java
@@ -0,0 +1,8 @@
+package programmers;
+
+public class P_더크게합치기 {
+	 public int solution(int a, int b) {
+	        
+	       return Math.max(Integer.parseInt(a + "" + b), Integer.parseInt(b + "" + a));
+	    }
+}
