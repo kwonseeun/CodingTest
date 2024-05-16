8f98b87704812a783204ed135074176b663baa3a
kwonseeun <0115km@naver.com>
2024-05-16 23:44:02 +0900

    P_원소들의 곱과 합

---
+++ CodingTest/src/programmers/P_원소들의_곱과합.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class P_원소들의_곱과합 {
+	 public int solution(int[] num_list) {
+	        int sum = 0;
+	        int mul = 1;
+	        for (int i = 0; i < num_list.length; i++) {
+	            sum += num_list[i];
+	            mul *= num_list[i];
+	        }
+
+	        if (mul < Math.pow(sum, 2)) {
+	            return 1;
+	        } else return 0;
+	   	  }
+}
