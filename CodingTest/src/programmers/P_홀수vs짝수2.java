192ba876e745070e51ca8d71e75b7fddaf2e6dd4
kwonseeun <0115km@naver.com>
2024-05-26 23:59:18 +0900

    P_홀수vs짝수

---
+++ CodingTest/src/programmers/P_홀수vs짝수2.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class P_홀수vs짝수2 {
+	 public int solution(int[] num_list) {
+	        int a = 0;
+	        int b = 0;
+
+	        for (int i = 0; i < num_list.length; i++) {
+	            if (i % 2 == 0){
+	                a += num_list[i];
+	            }else {
+	                b += num_list[i];
+	            }
+	        }
+	        return Math.max(a, b);
+	  	  }
+}
