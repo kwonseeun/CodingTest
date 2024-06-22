125553a85596577ee9b096e9c5a7eb6f4bd9eaee
kwonseeun <0115km@naver.com>
2024-06-22 09:59:34 +0900

    P_옹알이

---
+++ CodingTest/src/programmers/p_옹알이.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class p_옹알이 {
+	  public int solution(String[] babbling) {
+	        int answer = 0;
+	        for(int i=0; i< babbling.length; i++){
+	             babbling[i] = babbling[i].replaceFirst("aya", "0");
+	             babbling[i] = babbling[i].replaceFirst("ye", "0");
+	             babbling[i] = babbling[i].replaceFirst("woo", "0");
+	             babbling[i] = babbling[i].replaceFirst("ma", "0");
+//	    처음 부분을 포함한걸 전부 없에라 replace 할경우 다 없어지기 때문에 first사용
+	            babbling[i] = babbling[i].replace("0", ""); 
+	                  // 하나 없엔게 동일하게 하나가 더 남아있다면 그건 조합한거기 때문에 ++
+	              if(babbling[i].equals("")) answer++;
+	                
+	        }
+	                   
+	        return answer;
+	    }
+}
