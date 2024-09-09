493bf05c3d2ed56a5caf58e20e61856c31b4b146
kwonseeun <0115km@naver.com>
2024-09-09 07:11:44 -0700

    B_애너그램

---
+++ CodingTest/src/baekjoon/B_애너그램.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B_애너그램 {
+	public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        String ch1,ch2;
+        for(int i=0;i<n;i++){
+            ch1 = scanner.next();
+            ch2 = scanner.next();
+
+            char[] A = ch1.toCharArray();
+            char[] B = ch2.toCharArray();
+
+            Arrays.sort(A);
+            Arrays.sort(B);
+
+            if(Arrays.equals(A,B)){
+                System.out.println(ch1+ " & " +ch2+ " are anagrams.");
+            }else{
+                System.out.println(ch1+ " & " +ch2+ " are NOT anagrams.");
+            }
+        }
+    }
+}
