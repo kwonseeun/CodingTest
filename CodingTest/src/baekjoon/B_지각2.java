2acfc9f3498bc6d56897a2b57ad4138836246aab
kwonseeun <0115km@naver.com>
2024-09-12 07:20:25 -0700

    B_지각

---
+++ CodingTest/src/baekjoon/B_지각2.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B_지각2 {
+	public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int num, t;
+
+        for(int i=0;i<n;i++){
+
+            num = scanner.nextInt();
+            t=0;
+
+            while(true){
+
+                if( t*t+t > num){
+                    System.out.println(t-1);
+                    break;
+                }
+
+                t=t+1;
+            }
+        }
+    }
+}
