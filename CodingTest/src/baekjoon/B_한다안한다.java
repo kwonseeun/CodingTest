8d815fab63979a065499ce2a9480a40bdbdb0078
kwonseeun <0115km@naver.com>
2024-09-14 07:04:47 -0700

    B_한다안한다

---
+++ CodingTest/src/baekjoon/B_한다안한다.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B_한다안한다 {
+	 public static void main(String[] args){
+	        Scanner scanner = new Scanner(System.in);
+
+	        int n= scanner.nextInt();
+
+	        for(int i=0;i<n;i++){
+
+	            String ch=scanner.next();
+
+	            char[] N=ch.toCharArray();
+
+	            int NN=N.length/2;
+
+	            if(N[NN-1]==N[NN]){
+	                System.out.println("Do-it");
+	            }else{
+	                System.out.println("Do-it-Not");
+	            }
+	        }
+	    }
+}
