a30c86b633605264ff496d5d2932515c836c0176
kwonseeun <0115km@naver.com>
2024-12-10 06:34:46 -0800

    P_콜라츠추측

---
+++ CodingTest/src/programmers/P_콜라츠추측2.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class P_콜라츠추측2 {
+	public static int solution(int num) {
+        int answer = 0;
+        long number = (long) num;
+        if(number == 1) return 0;
+        while(number != 1) {
+            if(number % 2 == 0) {
+                number /= 2;
+            } else {
+                number = number*3+1;
+            }
+            answer++;
+
+            if (answer == 500) {
+                return -1;
+            }
+        }
+        return answer;
+    }
+}
