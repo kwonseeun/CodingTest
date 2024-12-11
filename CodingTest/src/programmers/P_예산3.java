3852d4eeff3f0c7857c4f4d46e0486054c3e8fb2
kwonseeun <0115km@naver.com>
2024-12-11 06:43:21 -0800

    P_예산

---
+++ CodingTest/src/programmers/P_예산3.java
@@ -0,0 +1,21 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class P_예산3 {
+	class Solution {
+	    public int solution(int[] d, int budget) {
+	        int answer = 0;
+	        Arrays.sort(d);
+	        for(int each : d) {
+	            if((budget - each) >= 0) {
+	                budget -= each;
+	                answer += 1;
+	            } else {
+	                break;
+	            }
+	        }
+	        return answer;
+	    }
+	}
+}
