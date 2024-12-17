91eff0024015a7c71fe5be98f42f2989b8fe1ced
kwonseeun <0115km@naver.com>
2024-12-17 05:48:56 -0800

    B_엄청난부자2

---
+++ CodingTest/src/baekjoon/B_엄청난부자2.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_엄청난부자2 {
+	public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer tk = new StringTokenizer(str, " ");
+        int money = Integer.parseInt(tk.nextToken());
+        int people = Integer.parseInt(tk.nextToken());
+
+        System.out.println(money/people);
+        System.out.println(money%people);
+    }
+    //test 1
+    //100 5
+    //20
+    //0
+
+    //test 2
+    //99999 9
+    //11111
+    //0
+	}
+}
+}
