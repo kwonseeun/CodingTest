fba06538f640c1576ed85ae4aeeafaf767c83d8b
kwonseeun <0115km@naver.com>
2024-12-18 06:45:34 -0800

    B_16진수 문제

---
+++ CodingTest/src/baekjoon/B_16진수문제.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B_16진수문제 {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        String num = sc.next();
+	        int ans = Integer.parseInt(num, 16);
+	        System.out.println(ans);
+	        //입력: A
+	        //출력: 10
+	    }
+}
